30056040d8f38c125b41d3de9ba0ea343428610f
zero <empdtj@gmail.com>
2026-02-12 00:37:56 +0500

    Update map-controller.ts

--- frontend/src/map/map-controller.ts
+++ frontend/src/map/map-controller.ts
@@ -21,7 +21,7 @@ export function initMap(): L.Map {
     "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
     {
       attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
+        '',
       subdomains: "abcd",
       maxZoom: 19,
     }
